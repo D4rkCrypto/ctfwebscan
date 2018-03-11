b75bc4a080c0d445cca69e2067427781fa264836
D4rk <swiftieterrence@outlook.com>
2018-03-11 22:56:02 +0800

    Better error handling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ TODO: 针对某些全站200的情况，分析返回数据包匹配"404 not found
 """
 
 import sys
+import argparse
 import requests
 
 class bcolors:
@@ -26,11 +27,15 @@ def dir_scan(url):
         lines = f.read().splitlines()
     f.close()
     for line in lines:
-        r = requests.get(url+line)
-        if r.status_code == 200 or r.status_code == 403:
-            print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + line)
-            # 如果扫描到存在的目录会进行敏感文件扫描
-            file_scan(url+line)
+        try:
+            r = requests.get(url+line, timeout=1)
+            if r.status_code == 200 or r.status_code == 403:
+                print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + line)
+                # 如果扫描到存在的目录会进行敏感文件扫描
+                file_scan(url+line)
+        except requests.exceptions.ConnectionError:
+            print("Error connecting to site \"%s\"" % url)
+            sys.exit(1)
 
 def file_scan(url):
     """扫描web敏感文件"""
@@ -39,14 +44,14 @@ def file_scan(url):
     f.close()
     for line in lines:
         try:
-            r = requests.get(url+line)
+            r = requests.get(url+line, timeout=1)
             if r.status_code == 200:
                 print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + line)
                 # 如果扫描到存在敏感文件会进行编辑器源码泄露扫描
                 source_scan(url, line)
-        except Exception:
+        except requests.exceptions.ConnectionError:
             print("Error connecting to site \"%s\"" % url)
-            exit()
+            sys.exit(1)
 
 def source_scan(url, filename):
     """扫描编辑器源码泄露"""
@@ -56,39 +61,41 @@ def source_scan(url, filename):
     leak4 = filename + '~'
     leaks = [leak1, leak2, leak3, leak4]
     for leak in leaks:
-        r = requests.get(url+leak)
-        if r.status_code == 200:
-            print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + leak)
+        try:
+            r = requests.get(url+leak, timeout=1)
+            if r.status_code == 200:
+                print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + leak)
+        except requests.exceptions.ConnectionError:
+            print("Error connecting to site \"%s\"" % url)
+            sys.exit(1)
 
 def main():
     """主函数"""
-    try:
-        if sys.argv[1].startswith('-'):
-            option = sys.argv[1][1:]
-            if option == 'f':
-                with open(sys.argv[2]) as f1:
-                    urls = f1.read().splitlines()
-                f1.close()
-                for url in urls:
-                    file_scan(url)
-                    dir_scan(url)
-                    exit()
-            elif option == 'u':
-                url = sys.argv[2]
-                file_scan(url)
-                dir_scan(url)
-                exit()
-            else:
-                print("Error: 未知参数")
-                exit()
-        else:
-            print("Usage: python main.py -u http://xxx.xxx/ 独立")
-            print("       python main.py -f xxx.txt 批量")
-            exit()
-    except Exception:
-        print("Usage: python main.py -u http://xxx.xxx/ 独立")
-        print("       python main.py -f xxx.txt 批量")
-        exit()
+    parser = argparse.ArgumentParser(description="CTF Web Scan")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-f", "--file", help="Specify file name")
+    group.add_argument("-u", "--url", help="Specify target url")
+    if len(sys.argv) == 1:
+        parser.print_usage(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    if args.url:
+        url = args.url
+        file_scan(url)
+        dir_scan(url)
+        print(bcolors.GREEN + 'Scan Complete: %s'%url + bcolors.ENDC)
+    elif args.file:
+        with open(args.file) as f1:
+            urls = f1.read().splitlines()
+        f1.close()
+        for url in urls:
+            file_scan(url)
+            dir_scan(url)
+            print(bcolors.GREEN + 'Scan Complete: %s'%url + bcolors.ENDC)
+        sys.exit(1)
+    else:
+        parser.print_usage(sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
